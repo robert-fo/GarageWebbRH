228bda71fe7e78169224f6ab32e95b1477aec4b1
robert-fo <robert.fo@gmail.com>
2016-02-11 10:11:16 +0100

    Initial Commit

---
+++ GarageWebbRH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarageWebbRH.Startup))]
+namespace GarageWebbRH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
