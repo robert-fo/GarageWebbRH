81de8016e821b8ed363c366a0257504338de7e0b
robert-fo <robert.fo@gmail.com>
2016-02-11 11:33:43 +0100

    Basklasser

---
+++ GarageWebbRH/Models/Fordon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageWebbRH.Models
+{
+    public class Fordon
+    {
+        [Key]
+        public string regNr { get; set; }
+        public enum fordonsTyp { Bil, MC, Buss, Lastbil }
+        public string agare { get; set; }
+        public DateTime pDatum { get; set; }
+        public DateTime startDatum { get; set; }
+        public DateTime slutDatum { get; set; }
+    }
+}
