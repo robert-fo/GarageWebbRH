7508b4087c1ddeda32d729f5b6eb586069654e98
robert-fo <robert.fo@gmail.com>
2016-02-12 16:18:15 +0100

    group by experimental

---
+++ GarageWebbRH/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageWebbRH.Models
+{
+    public class Group<K, T>
+    {
+        public K Key;
+        public IEnumerable<T> Values;
+    }
+}
